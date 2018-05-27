e24120ad29b0607b2962313f781d08dbbdc68bf5
Richard Sun <rdsun0301@gmail.com>
2018-05-26 21:20:59 -0700

    Changed speedFunction of albumFadeIn

--- public/js/albumFadeIn.js
+++ public/js/albumFadeIn.js
@@ -2,10 +2,14 @@ function albumFadeIn() {
 	$("div[class^='frame']").hide();
 
 	var numFrames = $("div[class^='frame']").length;
-
+	
 	for(let i=0; i < numFrames; i++) {
 		var className = '.frame' + i;
-		$(className).fadeIn(400*(i+1));
+
+		//How fast should the albums appear?
+		var speedFunction = 3000 - 1500*Math.pow(0.5, i);
+		
+		$(className).fadeIn(speedFunction);
 	}
 }
 
